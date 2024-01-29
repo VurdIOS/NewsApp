ca18fd8a55fcd9c9d0af1887d3b8b36447538bc4
VurdiOS <123585026+VurdIOS@users.noreply.github.com>
2024-01-29 20:54:57 +0300

    Initial Commit

---
+++ NewsApp/LogInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Камаль Атавалиев on 29.01.2024.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
